f59c42cff72f15e34daa4da40d08fb30c0a4cca2
Colin Bell <colin@dogoodercomics.co.uk>
2018-09-12 14:30:54 +0100

    Adds guess function to HiddenWord, tests passing

--- hiddenword.rb
+++ hiddenword.rb
@@ -1,3 +1,5 @@
+# require('pry-byebug')
+
 class HiddenWord
 
   attr_reader :answer, :display
@@ -7,6 +9,15 @@ class HiddenWord
     @display = word.chars.map { |letter| letter == " " ? " " : "*" }.join
   end
 
-
+  def guess(letter)
+    if @answer.include? letter
+      indices_of_letter = (0 ... @answer.length).find_all { |i| @answer[i] == letter }
+      indices_of_letter.each do |index|
+        @display[index] = @answer[index]
+      end
+    else
+      return false
+    end
+  end
 
 end
--- specs/hiddenword_spec.rb
+++ specs/hiddenword_spec.rb
@@ -9,16 +9,33 @@ class TestHiddenWord < Minitest::Test
     @sentence = HiddenWord.new("here is a sentence")
   end
 
-  def test_hiddenword_has_answer
+  def test_answer
     assert_equal("bananas", @word.answer)
   end
 
-  def test_hiddenword_has_display
+  def test_display
     assert_equal("*******", @word.display)
   end
 
-  def test_hiddenword_has_display__sentence
+  def test_display__sentence
     assert_equal("**** ** * ********", @sentence.display)
   end
+
+  def test_guess__single_letter
+    @word.guess("b")
+    assert_equal("b******", @word.display)
+  end
+
+  def test_guess__multiple_letters
+    @word.guess("a")
+    assert_equal("*a*a*a*", @word.display)
+  end
+
+  def test_guess__letter_not_found
+    result = @word.guess("z")
+    assert_equal(false, result)
+    assert_equal("*******", @word.display)
+  end
+
 
 end
