4afc3bf4fb48b15611bd1ee28b3199f67a5567fe
Colin Bell <colin@dogoodercomics.co.uk>
2018-09-13 21:29:38 +0100

    brings in random word generator

--- controller.rb
+++ controller.rb
@@ -1,13 +1,15 @@
 require('sinatra')
+require('random_word_generator')
 require_relative('models/game')
 require_relative('models/player')
 require_relative('models/hiddenword')
 
 def initialize
   super()
-  @player = Player.new("Colin")
-  @hiddenword = HiddenWord.new("Cheesecake")
-  @game = Game.new(@player, @hiddenword)
+  player = Player.new("Colin")
+  word = RandomWordGenerator.word
+  hiddenword = HiddenWord.new(word)
+  @game = Game.new(player, hiddenword)
 end
 
 get "/" do
@@ -17,7 +19,5 @@ end
 
 get "/:letter" do
   @game.guess(params[:letter])
-  @guessed = @game.guessed
-  @display = @game.display_word
   erb(:guess)
 end
