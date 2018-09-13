3d9bfdc49ffee781ad2246be5dfc83abc879bc69
Colin Bell <colin@dogoodercomics.co.uk>
2018-09-13 19:36:58 +0100

    adding alphabet buttons, setting up routes for guessed letters

--- controller.rb
+++ controller.rb
@@ -1,6 +1,20 @@
 require('sinatra')
+require_relative('models/game')
+require_relative('models/player')
+require_relative('models/hiddenword')
 
+player = Player.new("Colin")
+hiddenword = HiddenWord.new("Cheesecake")
+game = Game.new(player, hiddenword)
 
 get "/" do
+  @letters = ("a".."z")
   erb(:landing_page)
+end
+
+get "/:letter" do
+  game.guess(params[:letter])
+  @guessed = game.guessed
+  @display = game.display_word
+  erb(:guess)
 end 
