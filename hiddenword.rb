8d814be48a91018ac9d88a4e208e4d37f8d5c883
Colin Bell <colin@dogoodercomics.co.uk>
2018-09-13 09:49:41 +0100

    refactor hiddenword guess into easier to parse components

--- hiddenword.rb
+++ hiddenword.rb
@@ -9,12 +9,24 @@ class HiddenWord
     @display = word.chars.map { |letter| letter == " " ? " " : "*" }.join
   end
 
+  def answer_includes_letter?(letter)
+    return @answer.include? letter
+  end
+
+  def get_indexes_of_letter(letter)
+    return (0 ... @answer.length).find_all { |i| @answer[i] == letter }
+  end
+
+  def reveal_letters(indices)
+    indices.each do |index|
+      @display[index] = @answer[index]
+    end
+  end
+
   def guess(letter)
-    if @answer.include? letter
-      indices_of_letter = (0 ... @answer.length).find_all { |i| @answer[i] == letter }
-      indices_of_letter.each do |index|
-        @display[index] = @answer[index]
-      end
+    if answer_includes_letter?(letter)
+      indices_of_letter = get_indexes_of_letter(letter)
+      reveal_letters(indices_of_letter)
     else
       return false
     end
