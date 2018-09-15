0cf0a07be29404d5af7520bb9a70b3fd76421193
Colin Bell <colin@dogoodercomics.co.uk>
2018-09-15 23:38:46 +0100

    Controller and views refactored, letters now passed via POST requests

--- controller.rb
+++ controller.rb
@@ -5,37 +5,33 @@ require_relative('models/player')
 require_relative('models/hiddenword')
 require('pry')
 
-  player = Player.new("Colin")
-  word = RandomWordGenerator.word
-  hiddenword = HiddenWord.new(word)
-  $game = Game.new(player, hiddenword)
-
-get "/" do
+get "/welcome" do
   erb(:landing_page)
 end
 
-get "/guess/:letter" do
-  $game.guess(params[:letter])
-  redirect '/endgame' if $game.is_won? || $game.is_lost?
+post "/welcome" do
+  player = Player.new(params[:name])
+  word = RandomWordGenerator.word
+  p word
+  hiddenword = HiddenWord.new(word)
+  $game = Game.new(player, hiddenword)
+  redirect '/guess'
+end
 
+get "/guess" do
   @lives = $game.player.lives
   @letters = ("a".."z").select {|letter| letter unless $game.guessed.include? letter}
   erb(:guess)
 end
 
+post "/guess" do
+  $game.guess(params[:letter])
+  redirect '/endgame' if $game.is_won? || $game.is_lost?
+  redirect '/guess'
+end
+
 get "/endgame" do
-  binding.pry
   @outcome = $game.is_won? ? "You win!" : "You lose!"
   @answer = $game.show_answer
   erb(:endgame)
-end
-
-get "/newgame" do
-  player = Player.new("Colin")
-  word = RandomWordGenerator.word
-  hiddenword = HiddenWord.new(word)
-  $game = Game.new(player, hiddenword)
-  binding.pry
-  @letters = ("a".."z")
-  erb(:startgame)
 end
